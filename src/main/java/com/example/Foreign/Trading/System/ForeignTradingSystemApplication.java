737e652ffa272e9131c543e798503be4f5670e5e
ganthantm65 <tmganthan@gmail.com>
2026-02-13 21:29:07 +0530

    initialize

---
+++ src/main/java/com/example/Foreign/Trading/System/ForeignTradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Foreign.Trading.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignTradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignTradingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Foreign/Trading/System/ForeignTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Foreign.Trading.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignTradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
